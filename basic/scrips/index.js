39262ee657bd138b858300420306cc5451621bc2
lucyxinlu <84947231+lucyxinlu@users.noreply.github.com>
2021-08-22 19:27:59 -0400

    Update index.js

--- basic/scrips/index.js
+++ basic/scrips/index.js
@@ -1,1 +1,9 @@
-
+<!--create a button with callback>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "what's the capital of China?; Who is the president of USA?; What's the name of your university?";
+  document.getElementById("b2").innerHTML = "To see the answer, you need to click the answer";
+}
+ <!--create another button to distinguish the"answer" and "question">
+function button(){
+  document.getElementById("b3").innerHTML="Beijing; Biden; CMU ";
+  }
