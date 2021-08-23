1f9236982305cd9b9ce84fec18abcaffdec069e4
lucyxinlu <84947231+lucyxinlu@users.noreply.github.com>
2021-08-23 19:30:10 -0400

    Update index.js

--- basic/scrips/index.js
+++ basic/scrips/index.js
@@ -1,9 +1,39 @@
-<!--create a button with callback>
-function myFunction() {
-  document.getElementById("demo").innerHTML = "what's the capital of China?; Who is the president of USA?; What's the name of your university?";
-  document.getElementById("b2").innerHTML = "To see the answer, you need to click the answer";
+<script>
+class question{
+constructor(question,answer){
+this.question=question;
+this.answer=answer;}
 }
- <!--create another button to distinguish the"answer" and "question">
-function button(){
-  document.getElementById("b3").innerHTML="Beijing; Biden; CMU ";
-  }
+let myFlashcards=[];
+myFlashcards.push(new question('What is the capital of China?'));
+myFlashcards.push(new question('Who is the president of USA?'));
+myFlashcards.push(new question('What is the name of your university?'));
+	
+ /*Create the question array*/
+
+	let i=0;
+ function button(){
+	if(i<myFlashcards.length){
+document.getElementById("question").innerHTML=myFlashcards[i].question;	
+		 i++;
+	 } else if(i==myFlashcards.length){
+		 i=0;
+document.getElementById("question").innerHTML=myFlashcards[i].question;
+		 i++;
+	 }
+	 
+ }/*Get the question displayed one at a time*/
+	 
+let answer=new Array();
+answer.push("Beijing","Biden","CMU");
+//Create the answer array//
+	
+function buttonClicked(event){
+ document.getElementById("demo").innerHTML=answer;}
+//Define the onlick event//
+	
+function check(){
+	document.getElementById("alert").innerHTML='Click the question first';
+ }//have the mouseover event and print a message to users//
+</script>
+ 
